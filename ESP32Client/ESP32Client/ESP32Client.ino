5fe4e27b7d44cfc16ad28f877ac83dec96cf6ebb
juancamilo990526 <juancamiloperezc@gmail.com>
2022-09-25 20:15:33 -0500

    inicio de cambios para peticiones al servidor

---
+++ ESP32Client/ESP32Client/ESP32Client.ino
@@ -0,0 +1,58 @@
+#include <WiFiClient.h>
+#include <WiFiManager.h>
+
+// macros
+#define SERIAL_DEBUG // descomente esta linea si desea verificar datos en el puerto serie
+
+//prototipos
+void connectWiFi();
+
+// variables
+const char *ssidCaptive = "medio_artificial_esp32"; // se define nombre del SSID del portal cautivo
+const char *localHost = "127.0.0.1"; 
+const char *port = "3001"; 
+
+//objetos
+WiFiManager wifiManager;
+
+void setup() {
+  // se configura el modo de conexión a wifi
+  // como estación
+  WiFi.mode(WIFI_STA);
+
+  #ifdef SERIAL_DEBUG   
+   Serial.begin(115200); // se incializa el monitor serie
+  #endif
+}
+
+void loop() { 
+    connectWiFi();
+}
+
+
+// se conecta el wifi o se activa el portal cautivo en caso de desconexión
+void connectWiFi(){
+  if (!WiFi.status() == WL_CONNECTED) { // se verifica conexión a la red
+
+    #ifdef SERIAL_DEBUG
+     Serial.println("no conectado");
+    #endif
+
+    // se intenta conectar a la última red por defecto
+    wifiManager.autoConnect(ssidCaptive);
+    
+    #ifdef SERIAL_DEBUG
+      Serial.print("\n\nConectado a -> ");
+      Serial.println(WiFi.SSID());
+      Serial.print("ip -> " );
+      Serial.println(WiFi.localIP());
+      Serial.println("\n");
+    #endif
+  }else{ // si se conecta a la red
+    // se envían los datos de prueba al servidor 
+    #ifdef SERIAL_DEBUG
+     Serial.println("\nconsultando id del colino actual");
+    #endif
+  }
+}
+
